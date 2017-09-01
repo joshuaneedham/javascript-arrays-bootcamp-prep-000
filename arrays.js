4bacec3b452d6f5d8f0788b723d01433981dddd7
Joshua Needham <joshuabneedham@gmail.com>
2017-09-01 21:32:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-
 function addElementToBeginningOfArray(array, element) {
   var myArray = [1];
   myArray.unshift("foo");
