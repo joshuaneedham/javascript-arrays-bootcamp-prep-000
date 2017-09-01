3e37d8424b856e9a5f997c5db6fdbed12dd82d99
Joshua Needham <joshuabneedham@gmail.com>
2017-09-01 21:11:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  const myArray = [1];
+  var myArray = [1];
   myArray[0] = ['foo'];
   return myArray;
 }
