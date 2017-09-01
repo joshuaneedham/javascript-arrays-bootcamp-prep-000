a4f5503dec540a0625ca7e6967a14162e9023300
Joshua Needham <joshuabneedham@gmail.com>
2017-09-01 22:52:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -43,6 +43,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  var array2 = array.slice(array.length -1);
+  var array2 = array.slice(0, array.length -1);
   return array2;
 }
