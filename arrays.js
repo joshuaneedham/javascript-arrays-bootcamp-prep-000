e5c100c4a876a8cfe2d8ee9192e33fe1cab254e2
Joshua Needham <joshuabneedham@gmail.com>
2017-09-01 20:25:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,29 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
+function addElementToEndOfArray(array, element) {
+  
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  
+}
+function accessElementInArray(array, index) {
+  
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  
+}
+function removeElementFromBeginningOfArray(array) {
+  
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  
+}
+function removeElementFromEndOfArray(array) {
+  
+}
