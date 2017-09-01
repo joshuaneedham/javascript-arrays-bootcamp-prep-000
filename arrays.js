26ec92256184dc975cf9dc277ec4782c79af60da
Joshua Needham <joshuabneedham@gmail.com>
2017-09-01 22:27:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  var array = array.shift(array)
+  const array = array.shift();
   return array;
 }
 
