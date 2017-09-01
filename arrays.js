d1dd51f933ef7727f05b3519e7e7ffdcb2bcf9f4
Joshua Needham <joshuabneedham@gmail.com>
2017-09-01 20:56:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,7 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   var myArray = [1];
-  myArray("foo");
+  myArray = ("foo");
   return myArray;
 }
 function addElementToEndOfArray(array, element) {
